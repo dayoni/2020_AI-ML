24cb1f027fb926a86a72e3c56f54f0211ebc37d2
dayoni <kook981@gmail.com>
2020-11-12 19:34:07 +0900

    11.12 update

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# practice
+# 2020_AI-ML 프로젝트 개요
+------------
+본 프로젝트는 한국산업기술대학교 학부과정 중 **_<인공지능 설계 및 실습>**_ 과목에서 진행한 프로젝트입니다.
+
+## 데이터 집합 소개 및 출처
+------------
+본 프로젝트는 위스콘신 주립 대학교 병원에서 1989년 ~ 1991년 사이에 수집된 유방암 조직검사 결과를 이용하여 유방암을 진단하는 인공지능 시스템을 설계한다. 본 프로젝트를 위해 9가지의 측정 데이터와 양성 또는 악성으로 판단된 결과 데이터가 제공되며 그 데이터의 총 개수는 683개이다. 이번에 설계한 분류 시스템은 크게 5단계를 거쳐 설계되었다.
+
++ 데이터의 출처
+	+ UCI Machine Learning Repository
++ 데이터 제공자
+	+ Dr. WIlliam H. Wolberg (physician)
+		University of Wisconsin Hospitals
+		Madison, Wisconsin
+		USA
+	+ Donor: Olvi Mangasarian (mangasarian@cs.wisc.edu)
+		Received by David W. Aha (aha@cs.jhu.edu)
+		-- Date: 15 July 1992
+
+## 프로젝트 설명
+------------
++ 모델의 설계 및 구조와 흐름 
+	+ 본 프로젝트는 9입력 2클래스의 분류를 위한 시스템을 설계하는 것이 목적
+	1. 모델 : 시그모이드 함수(Sigmoid function)를 이용한 로지스틱 회귀 모델(Logistic-Regression)
+	2. 비용함수 : 교차 엔트로피 오차(CEE : Cross-Entropy_Error)
+	3. 최적화 : 켤레 경사 하강법(conjugate gradient method)
+	4. 검증 : K-Fold 교차검증(K-Fold Cross Validation)
+
+## 구조
+------------
++ Breast_cancer_data : 훈련 데이터 집합 (Training Data set)
++ Sample_test_data : 테스트 데이터 집합(Test Data set)
++ breastCancerClassification_dayoni : 9입력 2클래스 분류기
++ parameter : 비용함수인 CEE의 매개변수의 값
--- breastCancerClassification_dayoni.py
+++ breastCancerClassification_dayoni.py
@@ -1,49 +1,32 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[11]:
-
-
 import numpy as np
 import matplotlib.pyplot as plt
+
 get_ipython().run_line_magic('matplotlib', 'inline')
+from scipy.optimize import minimize
+
+data = np.loadtxt(fname="Breast_cancer_data.txt")  # 데이터 생성
+
+X = data[:, :9]  # 입력, 0열부터 8열까지
+T = data[:, 9]  # 출력, 9열만
+X = X.astype(int)  # 실수형으로 들어온 데이터를 정수형으로 변환
+T = T.astype(int)  # 실수형으로 들어온 데이터를 정수형으로 변환
+
+X_n = data.shape[0]  # data의 행의 갯수를 X_n에 대입
 
 
-# In[12]:
-
-
-data = np.loadtxt(fname = "Breast_cancer_data.txt")    #데이터 생성
-
-X = data[:, :9]      #입력, 0열부터 8열까지
-T = data[:, 9]       #출력, 9열만
-X = X.astype(int)    #실수형으로 들어온 데이터를 정수형으로 변환
-T = T.astype(int)    #실수형으로 들어온 데이터를 정수형으로 변환
-
-X_n = data.shape[0]  #data의 행의 갯수를 X_n에 대입
-
-
-# In[13]:
-
-
-def logistic9(x0, x1, x2, x3, x4, x5, x6, x7, x8, w):      #로지스틱 회귀 모델 생성
-    y = 1 / (1 + np.exp(-((w[0] * x0) + (w[1] * x1) + (w[2] * x2) + (w[3] * x3)+ (w[4] * x4)
+def logistic9(x0, x1, x2, x3, x4, x5, x6, x7, x8, w):  # 로지스틱 회귀 모델 생성
+    y = 1 / (1 + np.exp(-((w[0] * x0) + (w[1] * x1) + (w[2] * x2) + (w[3] * x3) + (w[4] * x4)
                           + (w[5] * x5) + (w[6] * x6) + (w[7] * x7) + (w[8] * x8) + w[9])))
     return y
 
 
-# In[14]:
-
-
-def cee_logistic9(w, x, t):                                #교차 엔트로피 오차(비용함수) 구하기
+def cee_logistic9(w, x, t):  # 교차 엔트로피 오차(비용함수) 구하기
     y = logistic9(x[:, 0], x[:, 1], x[:, 2], x[:, 3], x[:, 4], x[:, 5], x[:, 6], x[:, 7], x[:, 8], w)
     cee = 0
     for n in range(len(y)):
-        cee = cee - (t[n] * np.log(y[n]) + (1 - t[n]) * np.log(1 - y[n]))     
+        cee = cee - (t[n] * np.log(y[n]) + (1 - t[n]) * np.log(1 - y[n]))
     cee = cee / X_n
     return cee
-
-
-# In[15]:
 
 
 def dcee_logistic9(w, x, t):
@@ -63,65 +46,56 @@ def dcee_logistic9(w, x, t):
     dcee = dcee / X_n
     return dcee
 
+
 W = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 dcee_logistic9(W, X, T)
 
 
-# In[16]:
-
-
-from scipy.optimize import minimize
 def fit_logistic9(w_init, x, t):
-    res = minimize(cee_logistic9, w_init, args = (x, t),
-                  jac = dcee_logistic9, method = "CG")
+    res = minimize(cee_logistic9, w_init, args=(x, t),
+                   jac=dcee_logistic9, method="CG")
     return res.x
-
-
-# In[17]:
 
 
 W_init = [-1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 W = fit_logistic9(W_init, X, T)
-print("w0 = {0:.2f}, w1 = {1:.2f}, w2 = {2:.2f}, w3 = {3:.2f}, w4 = {4:.2f},      w5 = {5:.2f}, w6 = {6:.2f}, w7 = {7:.2f}, w8 = {8:.2f}, w9 = {9:.2f}"      .format(W[0], W[1], W[2],  W[3],  W[4],  W[5],  W[6], W[7],  W[8],  W[9]))
+print(
+    "w0 = {0:.2f}, w1 = {1:.2f}, w2 = {2:.2f}, w3 = {3:.2f}, w4 = {4:.2f},      w5 = {5:.2f}, w6 = {6:.2f}, w7 = {7:.2f}, w8 = {8:.2f}, w9 = {9:.2f}".format(
+        W[0], W[1], W[2], W[3], W[4], W[5], W[6], W[7], W[8], W[9]))
 
 
-# In[18]:
-
-
-def kfold_func(w_k, x, t, k):                    #x=측정데이터, t=결과데이터, k=몇개로 나눌것인가?
+def kfold_func(w_k, x, t, k):  # x=측정데이터, t=결과데이터, k=몇개로 나눌것인가?
     n = x.shape[0]
     cee_train = np.zeros(k)
     cee_test = np.zeros(k)
-    
-    for i in range(0, k):                        # i = 0, 1, 2 ... k-1
-        x_train = x[np.fmod(range(n), k) != i]   
+
+    for i in range(0, k):  # i = 0, 1, 2 ... k-1
+        x_train = x[np.fmod(range(n), k) != i]
         t_train = t[np.fmod(range(n), k) != i]
         x_test = x[np.fmod(range(n), k) == i]
         t_test = t[np.fmod(range(n), k) == i]
-        wm =fit_logistic9(w_k, x_train, t_train)
+        wm = fit_logistic9(w_k, x_train, t_train)
         cee_train[i] = cee_logistic9(wm, x_train, t_train)
         cee_test[i] = cee_logistic9(wm, x_test, t_test)
-    return cee_train, cee_test 
+    return cee_train, cee_test
+
 
 W_K = fit_logistic9(W_init, X, T)
 K = 40
 kfold_func(W_K, X, T, K)
 
 
-# In[10]:
-
-
 def validate_model(w):
-    d = np.loadtxt(fname = "Breast_cancer_data.txt")
-    X = d[:, :9].astype(int)
-    T = d[:, 9].astype(int)
-    
+    test = np.loadtxt(fname="Sample_test_data.txt")
+    X = test[:, :9].astype(int)
+    T = test[:, 9].astype(int)
+
     N = X.shape[0]
-    
+
     y = np.zeros(N)
     decision = np.zeros(N).astype(int)
     err_cnt = 0
-    
+
     print('No. \t Y \t T')
     print('--------------------')
     for i in range(N):
@@ -130,26 +104,24 @@ def validate_model(w):
         y[i] = 1 / (1 + np.exp(-u))
         if y[i] < 0.5:
             decision[i] = 1
-            
+
         if decision[i] != T[i]:
             err_cnt = err_cnt + 1
-            
+
         print('{0} \t {1} \t {2}'.format(i, decision[i], T[i]))
-        
+
     hit_ratio = np.round((1 - err_cnt / N) * 100, 1)
-    
+
     print('--------------------')
     print('Total error : {0} out of [1]'.format(err_cnt, N))
     print('Hit_ratio : {0:.1f} %'.format(hit_ratio))
-    
+
     return hit_ratio
 
-StudentID = "2018146015"
-W = np.loadtxt(fname = StudentID +".txt")
 
-print("Student ID : " + StudentID)
+W = np.loadtxt(fname="parameter.txt")
+
 print("W = " + np.str(W))
 print("\n")
 
 validate_model(W)
-
